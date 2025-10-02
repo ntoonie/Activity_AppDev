086a92f34f930502dac1ad768bcc422f68b56e96
ADMIN <ADMIN@MARKOY>
2025-10-02 21:54:30 +0800

    Add project files.

---
+++ Activity#1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("SIMPLE CALCULATOR");
+
+Console.WriteLine("Enter First Number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Second Number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Operation (+,-,*,/): ");
+string operation = Console.ReadLine();
+
+switch (operation)
+{
+    case "+":
+        Console.WriteLine($"Result: {num1 + num2}");
+        break;
+    case "-":
+        Console.WriteLine($"Result: {num1 - num2}");
+        break;
+    case "*":
+        Console.WriteLine($"Result: {num1 * num2}");
+        break;
+    case "/":
+        if (num2 != 0)
+            Console.WriteLine($"Result: {num1 / num2}");
+        else
+            Console.WriteLine("Error: Division by zero is not allowed.");
+        break;
+    default:
+        Console.WriteLine("Error: Invalid operation.");
+        break;
+}
+
+
+Console.ReadKey();
+
